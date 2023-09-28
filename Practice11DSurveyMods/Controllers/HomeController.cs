9b112495efd7b61db97710074040b68ea81e446b
DanielSCGray <138812633+DanielSCGray@users.noreply.github.com>
2023-09-28 14:57:42 -0700

    assignment dont

---
+++ Practice11DSurveyMods/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Practice11DSurveyMods.Models;
+
+namespace Practice11DSurveyMods.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+    
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+        public static Survey theSurvey;
+    [HttpPost("result/process")]
+    public IActionResult Process(Survey newSurvey)
+    {
+        theSurvey = newSurvey;
+        return RedirectToAction("Result");
+    }
+    [HttpGet("result")]
+    public IActionResult Result()
+    {
+        return View("Result", theSurvey);
+    }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Practice11DSurveyMods/Models/SurveyModel.cs
@@ -0,0 +1,13 @@
+#pragma warning disable CS8618
+
+namespace Practice11DSurveyMods.Models;
+
+public class Survey
+{
+    public string Name {get;set;}        
+    public string Location {get;set;} 
+    public string Language {get;set;} 
+    public string Comment {get;set;} 
+    
+    
+}
