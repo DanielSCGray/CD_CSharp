9fddecb29ba2fa045997f3b1b833622ae5a72818
DanielSCGray <138812633+DanielSCGray@users.noreply.github.com>
2023-09-29 13:49:56 -0700

    new assignment setup

---
+++ Practice12FormSub/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Practice12FormSub.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
